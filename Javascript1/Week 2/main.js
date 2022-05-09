758302299c5c256e1d90662e71dbdb79bc5f659d
Priscilla Silva <priscilla.sg91@gmail.com>
2022-05-09 20:48:15 +0200

    done with exercise 4

--- Javascript1/Week 2/main.js
+++ Javascript1/Week 2/main.js
@@ -1,7 +1,35 @@
+
 console.log('Hello world!');
 console.log('Hola mundo!');
 console.log('Hej världen!');
 
 console.log("I'm awesome"); 
-/*The error was that single quotes were used both to write the string and as an apostrophe. The
+/* The error was that single quotes were used both to write the string and as an apostrophe. The
 solution was to simply substitute the outside single quotes for double quotes */
+
+
+/* I'm only using var in this assignment because when I used const or let with an undeclared or repeated value it 
+would mark an error */
+
+var x
+console.log('The value of x will be: undefined');
+console.log(x);
+var x = 5;
+console.log('The value of x will be: 5');
+console.log(x);
+
+
+var y = 'Brtiney Spears';
+console.log('The value of y will be: Britney Spears');
+console.log(y);
+var y = 'Madonna';
+console.log('The value of y will be: Madonna');
+console.log(y);
+
+
+let z = 7.25;
+console.log(z);
+let a = Math.round(z);
+console.log(a);
+let b = Math.max(z, a);
+console.log(b);
