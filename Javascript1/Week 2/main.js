4665a24f6ddcc9713f869014633fa5f47c623421
Priscilla Silva <priscilla.sg91@gmail.com>
2022-05-09 20:19:16 +0200

    initial commit

---
+++ Javascript1/Week 2/main.js
@@ -0,0 +1,7 @@
+console.log('Hello world!');
+console.log('Hola mundo!');
+console.log('Hej världen!');
+
+console.log("I'm awesome"); 
+/*The error was that single quotes were used both to write the string and as an apostrophe. The
+solution was to simply substitute the outside single quotes for double quotes */
