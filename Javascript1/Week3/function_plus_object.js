5c62e884dec1cb94b8bcd667a6e639e47e07eedc
Priscilla Silva <priscilla.sg91@gmail.com>
2022-05-18 17:39:58 +0200

    done with the third function exercise

---
+++ Javascript1/Week3/function_plus_object.js
@@ -0,0 +1,12 @@
+const juanGabriel = {
+  name: "Juan Gabriel",
+  yearOfBirth: 1950,
+  countryOfOrigin: "Mexico",
+  occupation: "singer",
+  famousSong: "Querida"
+};
+
+function famousMexicanSinger() {
+  console.log(juanGabriel)
+}
+famousMexicanSinger();
---
+++ Javascript1/Week3/function_typeOfVehicle.js
@@ -0,0 +1,21 @@
+/* 
+Create a function named vehicleType that receives a color, and a code, 1 for car, 2 for motorbike. And prints 'a blue motorbike' for example when called as vehicleType("blue", 2)
+*/
+
+/* 1. Define a function vehicleType */
+/* 2. Equate car to 1 and motorbike to 2 */
+/* Allow for it to have two parameters: a color and a number, which will
+equate to a type of vehicle*/
+// **Might use early exit for it
+
+function vehicleType(color, vehicle) {
+  if(typeof color !== 'string' && typeof vehicle !== 'number')
+    return 'Color must a string and vehicle must be a number'
+  if(vehicle > 2)
+    return 'Vehicle can only be 1 or 2'
+  if(typeof color === 'string' && vehicle === 1)
+    return `A ${color} car`
+  if(typeof color === 'string' && vehicle === 2)
+    return `A ${color} motorcycle`
+}
+vehicleType('yellow', 2);
