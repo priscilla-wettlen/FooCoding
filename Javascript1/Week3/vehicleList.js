d6395bcd3975bccdf9659c99e9c3442e3bee6849
Priscilla Silva <priscilla.sg91@gmail.com>
2022-05-18 23:41:54 +0200

    finished 7 and 8

---
+++ Javascript1/Week3/vehicleList.js
@@ -0,0 +1,7 @@
+/*7. Make a list of vehicles, you can add "motorbike", "caravan", "bike", or more. */
+
+let vehicleList = ["motorbike", "caravan", "bike", "car", "scooter"]
+console.log(vehicleList);
+
+//How do you get the third element from that list?
+console.log(vehicleList[3]);
