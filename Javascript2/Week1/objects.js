2c5fa442122387d645b50d3240d9b31a56a9d9b2
Priscilla Silva <priscilla.sg91@gmail.com>
2022-05-23 22:24:41 +0200

    Prints the name on the screen, but in a very basic, ugly way. No format, no images.

--- Javascript2/Week1/objects.js
+++ Javascript2/Week1/objects.js
@@ -11,16 +11,21 @@ fetch("https://www.breakingbadapi.com/api/characters")
       return Math.floor(Math.random() * (max - min + 1) + min);
   
     }
-    console.log(getRandomIntInclusive(0, 62));
+    //console.log(getRandomIntInclusive(0, 62));
     
     const button = document.getElementById('random_character');
+    const container = document.getElementById('container');
 
     //Connected the function to the button. Random number/character shows in the console
     button.onclick = function () {
       
-      console.log(getRandomIntInclusive(0, 62));
+      //console.log(getRandomIntInclusive(0, 62));
       //Log the result appended to the 'data' and it gives you a random chatacter info
       console.log(data[getRandomIntInclusive(0, 62)]);
+
+      //Prints the name on the screen, but in a very basic, ugly way. No format, no images.
+      container.append(data[getRandomIntInclusive(0, 62)].name);
+
     }
     
     
