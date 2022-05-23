b90bb7725882cb9cda3ae05428904edfbe1fd24f
Priscilla Silva <priscilla.sg91@gmail.com>
2022-05-23 21:06:11 +0200

    connected function for random character to the button. for now it just show on the console

--- Javascript2/Week1/objects.js
+++ Javascript2/Week1/objects.js
@@ -2,21 +2,27 @@ fetch("https://www.breakingbadapi.com/api/characters")
   .then(res => res.json())
   .then(data => {
 
-  console.log(data);
-    //Function to get a random integer between two speciified values
+  //console.log(data);
+
+     //Function to get a random integer between two speciified values
     function getRandomIntInclusive(min, max) {
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1) + min);
   
-
     }
-
     console.log(getRandomIntInclusive(0, 62));
     
-    //Log the result appended to the 'data' and it gives you a random chatacter info
-    console.log(data[getRandomIntInclusive(0, 62)]); 
+    const button = document.getElementById('random_character');
 
+    //Connected the function to the button. Random number/character shows in the console
+    button.onclick = function () {
+      
+      console.log(getRandomIntInclusive(0, 62));
+      //Log the result appended to the 'data' and it gives you a random chatacter info
+      console.log(data[getRandomIntInclusive(0, 62)]);
+    }
+    
     
   });
 
