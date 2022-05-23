060a4fba4c15df555137ce2a343da920820ac3aa
Priscilla Silva <priscilla.sg91@gmail.com>
2022-05-23 15:19:31 +0200

    created function to get  a random character

--- Javascript2/Week1/objects.js
+++ Javascript2/Week1/objects.js
@@ -2,9 +2,21 @@ fetch("https://www.breakingbadapi.com/api/characters")
   .then(res => res.json())
   .then(data => {
 
+  console.log(data);
+    //Function to get a random integer between two speciified values
+    function getRandomIntInclusive(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min);
   
-    console.log(data[1])
-    console.log(data.length)
-    console.log(data[50].occupation)
 
-});
+    }
+
+    console.log(getRandomIntInclusive(0, 62));
+    
+    //Log the result appended to the 'data' and it gives you a random chatacter info
+    console.log(data[getRandomIntInclusive(0, 62)]); 
+
+    
+  });
+
