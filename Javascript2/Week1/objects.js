566fb6a16723908350bad874400530c8e41a850e
Priscilla Silva <priscilla.sg91@gmail.com>
2022-05-24 16:14:36 +0200

    made character and image match

--- Javascript2/Week1/objects.js
+++ Javascript2/Week1/objects.js
@@ -2,37 +2,74 @@ fetch("https://www.breakingbadapi.com/api/characters")
   .then(res => res.json())
   .then(data => {
 
-  //console.log(data);
+    console.log(data);
 
-     //Function to get a random integer between two speciified values
-    function getRandomIntInclusive(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1) + min);
+    //Function to get a random integer between two speciified values
+    // function getRandomIntInclusive(min, max) {
+    //   min = Math.ceil(min);
+    //   max = Math.floor(max);
+    //   return Math.floor(Math.random() * (max - min + 1) + min);
   
-    }
+    // }
     //console.log(getRandomIntInclusive(0, 62));
     
+    // const button = document.getElementById('random_character');
+    // const container = document.getElementById('container');
+    // const name = document.querySelector('.name');
+    // const image = document.createElement('img');
+
+    //Connected the function to the button. Random number/character shows in the console
+    // button.onclick = function () {
+      
+    //console.log(getRandomIntInclusive(0, 62));
+    //Log the result appended to the 'data' and it gives you a random chatacter info
+    //console.log(data[getRandomIntInclusive(0, 62)]);
+
+    //Appended to a p element and now it appears in the format name: bla blla
+    // name.innerText = `Name: ${(data[getRandomIntInclusive(0, 62)].name)}`;
+    // image.setAttribute("src", (data[getRandomIntInclusive(0, 62)].img));
+    // document.querySelector(".image").appendChild(image);
+
+
+    //}
+
+    let characters = [];
+    characters = data;
+
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+     max = Math.floor(max);
+       return Math.floor(Math.random() * (max - min + 1) + min);
+  
+      }
+    
+    //How to access the key char_id
+    //x = console.log(characters[getRandomIntInclusive(0,62)].name);
+
+    
+    
+    // function getRandomIntInclusive(min, max) {
+    //    min = Math.ceil(min);
+    //    max = Math.floor(max);
+    //    return Math.floor(Math.random() * (max - min + 1) + min);
+  
+    //  }
+  
     const button = document.getElementById('random_character');
     const container = document.getElementById('container');
     const name = document.querySelector('.name');
     const image = document.createElement('img');
 
-    //Connected the function to the button. Random number/character shows in the console
     button.onclick = function () {
-      
-      //console.log(getRandomIntInclusive(0, 62));
-      //Log the result appended to the 'data' and it gives you a random chatacter info
-      //console.log(data[getRandomIntInclusive(0, 62)]);
-
-      //Appended to a p element and now it appears in the format name: bla blla
-      name.innerText = `Name: ${(data[getRandomIntInclusive(0, 62)].name)}`;
-      image.setAttribute("src", (data[getRandomIntInclusive(0, 62)].img));
+      const randomChar = data[getRandomIntInclusive(0, 62)].char_id;
+      name.innerText = `Name: ${(data[randomChar].name)}`;
+      console.log(randomChar);
+      image.setAttribute("src", (data[randomChar].img));
       document.querySelector(".image").appendChild(image);
 
+     }
+    
+    
 
-    }
-    
-    
   });
 
