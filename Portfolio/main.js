c4100b347a36ad75345f457bc0022af26d00303c
Priscilla Silva <priscilla.sg91@gmail.com>
2022-07-24 14:29:16 +0200

    made images of carousel appear next to each other

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -16,11 +16,16 @@ const prevButton = document.querySelector('.right');
 const dotNav = document.querySelector('.carousel__nav')
 const dots = Array.from(dotNav.children);
 
-const slideSize = slides[0].getBoundingClientRect();
-const slideWidth = slideSize.width;
+const slideWidth = slides[0].getBoundingClientRect().width;
 
-console.log(slideSize)
-console.log(slideWidth)
+//arrange slides to be next to each other
+slides[0].style.left = slideWidth * 0 + 'px';
+slides[1].style.left = slideWidth * 1 + 'px';
+slides[2].style.left = slideWidth * 2 + 'px';
+slides[3].style.left = slideWidth * 3 + 'px';
+slides[4].style.left = slideWidth * 4 + 'px';
+
+
 //when i click left, move slides to the left
 //when i click right, move slides to the right
 //when i click indicators, move to that slide
